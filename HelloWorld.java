688f2ad9699df331cc1f29e5429b96e797ef96e5
shujoshi <shubhamjoshi12@yahoo.in>
2019-04-13 14:01:24 +0530

    updates

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package javaLearning;
+
+public class HelloWorld {
+static int x=10;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello World");
+      
+	}
+	
+	public void sum() {
+		System.out.println(x);
+	}
+
+}
