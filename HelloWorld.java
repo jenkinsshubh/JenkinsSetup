48269e8c99602cdf55201096d21ebce52637ac58
shujoshi <shubhamjoshi12@yahoo.in>
2019-04-13 14:34:26 +0530

    updates

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ static int x=10;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello World");
-      
+      System.out.println("x is: " + x);
 	}
 	
 	public void sum() {
